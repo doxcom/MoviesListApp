349c8c23e5212046e3a9a0f29206868bb14f5d7a
doxcom <aldoperez1319@gmail.com>
2026-07-10 01:15:56 -0600

    project 2 finished

---
+++ moviemaniac/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import "./App.css";
+import Navbar from './components/Navbar/Navbar';
+import MovieList from './components/MovieList/MovieList';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+         <main> 
+           <MovieList />
+         </main>
+    </div>
+  )
+}
+
+export default App
